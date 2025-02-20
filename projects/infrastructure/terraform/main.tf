500cde6762a7e593cff3232d58b35fcc198d64da
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-20 14:56:17 +0100

    Created initial file for the infrastructure.

---
+++ projects/infrastructure/terraform/main.tf
@@ -0,0 +1,25 @@
+# provider config
+terraform {
+  cloud {
+    organization = "rondon-sarnik"
+
+    workspaces {
+      name = "lkr-general-infrastructure"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.45.0"
+    }
+  }
+}
+
+variable "cloudflare_api_token" {
+  default = ""
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
