1289ada075eb07ef330edfad24f600abb9773128
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-21 11:13:50 +0100

    Initial code to create an api.

---
+++ bases/server/src/app/server/core.cljs
@@ -0,0 +1,27 @@
+(ns app.server.core
+  (:require [reitit.core :as r]
+            [app.server.async :refer [js-await]]
+            [app.server.cf :as cf :refer [defclass]]))
+
+(def router
+  (r/router
+   ["/api"
+    ["/todos" ::todos]]))
+
+;; args:
+;;  route: Reitit route data
+;;  request: js/Request object https://developers.cloudflare.com/workers/runtime-apis/request/
+;;  env: Environment object containing env vars and bindings to Cloudflare services https://developers.cloudflare.com/workers/configuration/environment-variables/
+;;  ctx: The Context API provides methods to manage the lifecycle of your Worker https://developers.cloudflare.com/workers/runtime-apis/context/
+(defmulti handle-route (fn [route request env ctx]
+                         [(-> route :data :name) (keyword (.-method ^js request))]))
+
+(defmethod handle-route [::todos :GET] [route request env ctx]
+  (js-await [{:keys [success results]} {:sucess true :results {:hello :world}}]
+            (if success
+              (cf/response-edn {:result results} {:status 200})
+              (cf/response-error))))
+
+;; entry point
+(def handler
+  #js {:fetch (cf/with-handler router handle-route)})
