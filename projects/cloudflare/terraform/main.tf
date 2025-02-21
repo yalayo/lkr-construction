584fab6fd42ced0666793addfa3704a2cfd8ce74
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-21 13:35:20 +0100

    Updated the file name in terraform.

--- projects/cloudflare/terraform/main.tf
+++ projects/cloudflare/terraform/main.tf
@@ -27,5 +27,5 @@ provider "cloudflare" {
 resource "cloudflare_workers_script" "score_backend" {
   account_id = "4180cebb0f75e57d434f27a622da70f0"
   name       = "lkr-app"
-  content    = file("server.js")
+  content    = file("main.js")
 }
