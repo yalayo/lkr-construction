29784357a6c9c3d356fa81bba7df0ce358ba4ca6
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-21 13:03:48 +0100

    Added code to create a worker.

---
+++ projects/cloudflare/terraform/main.tf
@@ -0,0 +1,31 @@
+# provider config
+terraform {
+  cloud {
+    organization = "rondon-sarnik"
+
+    workspaces {
+      name = "lkr-app"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.45.0"
+    }
+  }
+}
+
+variable "cloudflare_api_token" {
+  default = ""
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_workers_script" "score_backend" {
+  account_id = "4180cebb0f75e57d434f27a622da70f0"
+  name       = "lkr-app"
+  content    = file("server.js")
+}
