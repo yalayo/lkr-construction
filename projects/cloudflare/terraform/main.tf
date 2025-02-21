d2beed1dc9b3dfaf8d0f110e048b2907999438fc
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-21 13:47:34 +0100

    Add module attribute.

--- projects/cloudflare/terraform/main.tf
+++ projects/cloudflare/terraform/main.tf
@@ -28,5 +28,6 @@ resource "cloudflare_workers_script" "score_backend" {
   account_id = "4180cebb0f75e57d434f27a622da70f0"
   name       = "lkr-app"
   compatibility_date = "2024-01-01"
+  module     = true
   content    = file("main.js")
 }
