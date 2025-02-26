2d0b92a00a77236a91a4e6a7d4be43072d85ab87
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-26 12:54:40 +0100

    Temporarily removed this resources.

--- projects/infrastructure/terraform/domain.tf
+++ projects/infrastructure/terraform/domain.tf
@@ -1,10 +1,10 @@
-resource "cloudflare_pages_project" "pages_project" {
-  account_id = "4180cebb0f75e57d434f27a622da70f0"
-  name = "lkrconstruction"
-}
+# resource "cloudflare_pages_project" "pages_project" {
+#   account_id = "4180cebb0f75e57d434f27a622da70f0"
+#   name = "lkrconstruction"
+# }
 
-resource "cloudflare_pages_domain" "frontend" {
-  account_id = "4180cebb0f75e57d434f27a622da70f0"
-  project_name = cloudflare_pages_project.pages_project.id
-  name = "lkrconstruction.com"
-}
+# resource "cloudflare_pages_domain" "frontend" {
+#   account_id = "4180cebb0f75e57d434f27a622da70f0"
+#   project_name = cloudflare_pages_project.pages_project.id
+#   name = "lkrconstruction.com"
+# }
