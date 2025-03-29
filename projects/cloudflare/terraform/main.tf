60fac45588dc69a06128678d432a420b3d5ae30d
Yunior Alayo <yunior.alayo@gmail.com>
2025-03-29 15:11:57 +0100

    Removed the account id.

--- projects/cloudflare/terraform/main.tf
+++ projects/cloudflare/terraform/main.tf
@@ -33,7 +33,7 @@ provider "cloudflare" {
 }
 
 resource "cloudflare_workers_script" "score_backend" {
-  account_id = var.cloudflare_account_id # "4180cebb0f75e57d434f27a622da70f0"
+  account_id = var.cloudflare_account_id
   name       = "lkr-app"
   compatibility_date = "2024-01-01"
   module     = true
